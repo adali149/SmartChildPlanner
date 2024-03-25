cb4c6285fe2099909663fda787c541028663e85b
ABDULLAH KIRMIZI <abdullah.kirmizi@turkcell.com.tr>
2024-03-25 19:47:31 +0300

    Initial Commit

---
+++ SmartChildPlanner/SmartChildPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartChildPlannerApp.swift
+//  SmartChildPlanner
+//
+//  Created by ABDULLAH KIRMIZI on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartChildPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
